b834b5b7d7d3556652538748fe124f362049089c
Erandir <aefs12junior@gmail.com>
2018-10-27 17:37:51 -0300

    Http classes

--- src/Http/HttpRequest.php
+++ src/Http/HttpRequest.php
@@ -1,15 +1,51 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: erandir
- * Date: 06/10/18
- * Time: 12:38
- */
 
 namespace PlugRoute\Http;
 
+use PlugRoute\Helpers\PlugHelper;
 
 class HttpRequest
 {
+	private $body;
 
+	public function getBody($index = null)
+	{
+		if (is_null($index)) {
+			return $this->body;
+		}
+
+		return $this->body[$index];
+	}
+
+	public function getFiles()
+	{
+		return $_FILES;
+	}
+
+	public function getMethod()
+	{
+		return PlugHelper::getTypeRequest();
+	}
+
+	public function getHeaders()
+	{
+
+	}
+
+	private function getRequest() {
+		$typeRequest = $this->getMethod();
+
+		switch ($typeRequest) {
+			case 'GET' :
+				break;
+			case 'POST' :
+				break;
+			case 'PUT' :
+				break;
+			case 'DELETE' :
+				break;
+			case 'PATCH' :
+				break;
+		}
+	}
 }
--- src/Http/HttpResponse.php
+++ src/Http/HttpResponse.php
@@ -11,5 +11,29 @@ namespace PlugRoute\Http;
 
 class HttpResponse
 {
+	private $header;
 
+	public function __construct()
+	{
+		$this->header['statusCode'] = 200;
+		$this->header['contentType'] = 'application/json';
+	}
+
+	public function setStatus($statusCode)
+	{
+		$this->header['statusCode'] = $statusCode;
+	}
+
+	public function setContentType($contentType)
+	{
+		$this->header['contentType'] = $contentType;
+	}
+
+	public function send($data)
+	{
+		header("Content-type: {$this->header['contentType']}");
+		header("HTTP/1.0 {$this->header}");
+
+		return json_encode($data);
+	}
 }
