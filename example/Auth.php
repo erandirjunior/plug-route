40c20e413ac7cd06d0e8c2bd2225a6f0edd39b02
Erandir <aefs12junior@gmail.com>
2019-03-20 21:11:39 -0300

    Fix parameter type

--- example/Auth.php
+++ example/Auth.php
@@ -2,9 +2,8 @@
 
 class Auth implements \PlugRoute\Middleware\PlugRouteMiddleware
 {
-	public function handle($request): \PlugRoute\Http\Request
+	public function handle(\PlugRoute\Http\Request $request): \PlugRoute\Http\Request
 	{
-		var_dump($request->all());
 		return $request;
 	}
 }
--- example/OtherMiddleware.php
+++ example/OtherMiddleware.php
@@ -2,7 +2,7 @@
 
 class OtherMiddleware implements \PlugRoute\Middleware\PlugRouteMiddleware
 {
-	public function handle($request): \PlugRoute\Http\Request
+	public function handle(\PlugRoute\Http\Request $request): \PlugRoute\Http\Request
 	{
 		$request->setBody(['middleware' => 'settting value']);
 		return $request;
