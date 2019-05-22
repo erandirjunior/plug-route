e390346f71c3fe0719b17e1e944908eb4a3a4afc
Erandir <aefs12junior@gmail.com>
2019-05-21 21:33:54 -0300

    Update example

--- example/index.php
+++ example/index.php
@@ -65,7 +65,7 @@ $route->group(['prefix' => '/department', 'middlewares' => [OtherMiddleware::cla
 	$route->get('/tecnology', function(Request $request) {
 		$request->redirectToRoute('ti');
 
-		// If you use this library without name a route, without virtualhost or php server built-in
+		// If you use this library without name a route, without v'irtualhost or php server built-in
 		// use the redirect method
 		//$request->redirect('http://localhost/plug-route/example/department/it');
 	});
