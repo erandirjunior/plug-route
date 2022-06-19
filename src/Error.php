9acee7b565a97281fc2852825dbd2e3a9fdd6a25
Erandir <antonio.soares@multilaser.com.br>
2022-06-19 17:38:56 -0300

    Added error handler

--- src/Error.php
+++ src/Error.php
@@ -2,10 +2,12 @@
 
 namespace PlugRoute;
 
+use Exception;
+
 class Error
 {
 	public static function throwException(string $message)
 	{
-		throw new \Exception($message);
+		throw new Exception($message);
 	}
 }
